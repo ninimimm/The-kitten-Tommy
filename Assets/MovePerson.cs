62d35135c8d45b783fb992c8854d3d11888a034c
ninimimm <nikitachernov89221156246@yandex.ru>
2023-04-16 01:23:07 +0500

    Don't double jump

--- Assets/MovePerson.cs
+++ Assets/MovePerson.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fad58d734bc6cc2cdf7b0a0a977e72bfa8a6e8ba2c2cb1a246fb1ba44d5ae71
-size 1550
+oid sha256:4cd588c5c404b490b49d4789c7c84f91712189ca50c424172ebb249bf616cb9e
+size 1921
