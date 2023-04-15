5cfa1b180505017d4723eb9ecb2cae7432c438df
ninimimm <nikitachernov89221156246@yandex.ru>
2023-04-15 19:42:02 +0500

    Collider

--- Assets/MovePerson.cs
+++ Assets/MovePerson.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c7ee7dff0f7a4b6fa9b342edb6d363037f1f267bb0ecb97419ab9692f0e3c5f
-size 1779
+oid sha256:71eef30590f7a9866942fd696021b8c41fbdfef7d8b29f857ab6ff900b84e719
+size 1781
