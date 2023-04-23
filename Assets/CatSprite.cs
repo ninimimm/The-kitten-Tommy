89dcd5cbb9fc337a02e361f43f525f6238e8ba0e
ninimimm <nikitachernov89221156246@yandex.ru>
2023-04-23 15:41:44 +0500

    Good animations for Cat

--- Assets/CatSprite.cs
+++ Assets/CatSprite.cs
@@ -2,83 +2,87 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using SnakeTarget;
 using UnityEngine;
 
-public class CatSprite : MonoBehaviour
+namespace Cat
 {
-    // Start is called before the first frame update
-    // Add a jumpForce variable to control the jump strength
-    private Rigidbody2D _rb; // Add a RigidBody variable
-    private float move;
-    private MovementState _state;
-    private bool rotation = true;
-    private Animator _animator;
-    private enum MovementState { Stay, Run, jumpup, jumpdown, hit, damage };
-    private SpriteRenderer Cat;
-    private Animator _snakeAnimator;
-    [SerializeField] private float HP = 10;
-    [SerializeField] public float speed = 100.0f;
-    [SerializeField] public float jumpForce = 1f;
-    [SerializeField] public GameObject Snake;
-    private void Start()
+    public class CatSprite : MonoBehaviour
     {
-        _rb = GetComponent<Rigidbody2D>(); // Initialize the RigidBody variable
-        Cat = GetComponent<SpriteRenderer>();
-        _animator = GetComponent<Animator>();
-        _snakeAnimator = Snake.GetComponent<Animator>();
-        transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
-    }
-
-    // Add a new method to check if the character is grounded
-    private void Update()
-    {
-        move = Input.GetAxisRaw("Horizontal");
-        transform.position += new Vector3(move, 0, 0)*speed*Time.deltaTime;
-        if (Input.GetButtonDown("Jump") && CanJump())
-            _rb.AddForce(new Vector2(0,jumpForce),ForceMode2D.Impulse);
-        SwitchAnimation();
-    }
-
-    private bool CanJump() => _state != MovementState.jumpup && _state != MovementState.jumpdown;
-
-    private void SwitchAnimation()
-    {
-        if (move > 0)
+        // Start is called before the first frame update
+        // Add a jumpForce variable to control the jump strength
+        private Rigidbody2D _rb; // Add a RigidBody variable
+        private float move;
+        public static MovementState _stateCat;
+        private bool rotation = true;
+        private Animator _animator;
+        public enum MovementState { Stay, Run, jumpup, jumpdown, hit, damage };
+        private SpriteRenderer Cat;
+        private Animator _snakeAnimator;
+        [SerializeField] private float HP = 10;
+        [SerializeField] public float speed = 100.0f;
+        [SerializeField] public float jumpForce = 1f;
+        [SerializeField] public GameObject Snake;
+        private void Start()
         {
-            _state = MovementState.Run;
-            if (rotation)
+            _rb = GetComponent<Rigidbody2D>(); // Initialize the RigidBody variable
+            Cat = GetComponent<SpriteRenderer>();
+            _animator = GetComponent<Animator>();
+            _snakeAnimator = Snake.GetComponent<Animator>();
+            transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+        }
+
+        // Add a new method to check if the character is grounded
+        private void Update()
+        {
+            move = Input.GetAxisRaw("Horizontal");
+            transform.position += new Vector3(move, 0, 0)*speed*Time.deltaTime;
+            if (Input.GetButtonDown("Jump") && CanJump())
+                _rb.AddForce(new Vector2(0,jumpForce),ForceMode2D.Impulse);
+            SwitchAnimation();
+        }
+
+        private bool CanJump() => _stateCat != MovementState.jumpup && _stateCat != MovementState.jumpdown;
+
+        private void SwitchAnimation()
+        {
+            if (move > 0)
             {
-                transform.localScale = new Vector3(-transform.localScale.x,transform.localScale.y,transform.localScale.y);
-                rotation = false;
+                _stateCat = MovementState.Run;
+                if (rotation)
+                {
+                    transform.localScale = new Vector3(-transform.localScale.x,transform.localScale.y,transform.localScale.y);
+                    rotation = false;
+                }
             }
-        }
-        else if (move < 0)
-        {
-            _state = MovementState.Run;
-            if (!rotation)
+            else if (move < 0)
             {
-                transform.localScale = new Vector3(-transform.localScale.x,transform.localScale.y,transform.localScale.y);
-                rotation = true;
+                _stateCat = MovementState.Run;
+                if (!rotation)
+                {
+                    transform.localScale = new Vector3(-transform.localScale.x,transform.localScale.y,transform.localScale.y);
+                    rotation = true;
+                }
             }
+            else
+                _stateCat = MovementState.Stay;
+            if (_rb.velocity.y > .001f)
+                _stateCat = MovementState.jumpup;
+            else if (_rb.velocity.y < -.001f)
+                _stateCat = MovementState.jumpdown;
+            if (Vector2.Distance(transform.position,Snake.transform.position) < 1 && ComponentSnake._stateSnake == ComponentSnake.MovementState.attake)
+            {
+                _stateCat = MovementState.damage;
+                HP -= 1;
+            }
+            if (Input.GetButton("Fire1"))
+                _stateCat = MovementState.hit;
+            if (HP < 0)
+            {
+                HP = 100;
+                transform.position = new Vector3(1, -0.6f, 0);
+            }
+            _animator.SetInteger("State", (int)_stateCat);
         }
-        else
-            _state = MovementState.Stay;
-        if (_rb.velocity.y > .001f)
-            _state = MovementState.jumpup;
-        else if (_rb.velocity.y < -.001f)
-            _state = MovementState.jumpdown;
-        if (Vector2.Distance(transform.position,Snake.transform.position) < 1)
-        {
-            _state = MovementState.damage;
-            HP -= 1;
-        }
-        if (Input.GetButton("Fire1"))
-            _state = MovementState.hit;
-        if (HP == 0)
-        {
-            HP = 100;
-            transform.position = new Vector3(1, -0.6f, 0);
-        }
-        _animator.SetInteger("State", (int)_state);
     }
 }
--- Assets/ComponentSnake.cs
+++ Assets/ComponentSnake.cs
@@ -1,63 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using Cat;
 using UnityEngine;
 
-public class ComponentSnake : MonoBehaviour
+namespace SnakeTarget
 {
-    [SerializeField] private float HP = 3;
-    [SerializeField] private GameObject target;
-    private Animator _animator;
-    private enum MovementState { Stay, Walk, attake, death, hurt };
-    private MovementState _state;
-    private SpriteRenderer _snake;
-    private PolygonCollider2D _col;
-    private Animator targetAnimation;
-
-    private Rigidbody2D _rb;
-    // Start is called before the first frame update
-    void Start()
+    public class ComponentSnake : MonoBehaviour
     {
-        _rb = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
-        _snake = GetComponent<SpriteRenderer>();
-        _col = GetComponent<PolygonCollider2D>();
-        targetAnimation = target.GetComponent<Animator>();
-    }
+        [SerializeField] private float HP = 3;
+        [SerializeField] private GameObject target;
+        private Animator _animator;
+        public enum MovementState { Stay, Walk, attake, death, hurt };
+        public static MovementState _stateSnake;
+        private SpriteRenderer _snake;
+        private PolygonCollider2D _col;
+        private Animator targetAnimation;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (transform.position.x > 12.7)
-            transform.position = new Vector3(12.69f,transform.position.y,transform.position.z);
-        if (HP < 0)
+        private Rigidbody2D _rb;
+        // Start is called before the first frame update
+        void Start()
         {
-            _state = MovementState.death;
-            transform.position = new Vector3(5, -0.6f, 0);
+            _rb = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+            _snake = GetComponent<SpriteRenderer>();
+            _col = GetComponent<PolygonCollider2D>();
+            targetAnimation = target.GetComponent<Animator>();
         }
-        if (_rb.velocity.x > 0)
+
+        // Update is called once per frame
+        void Update()
         {
-            _state = MovementState.Walk;
-            _snake.flipX = true;
+            if (transform.position.x > 12.7)
+                transform.position = new Vector3(12.69f,transform.position.y,transform.position.z);
+            if (HP < 0)
+            {
+                _stateSnake = MovementState.death;
+                transform.position = new Vector3(5, -0.6f, 0);
+            }
+            if (_rb.velocity.x > 0)
+            {
+                _stateSnake = MovementState.Walk;
+                _snake.flipX = true;
+            }
+            else if (_rb.velocity.x < 0)
+            {
+                _stateSnake = MovementState.Walk;
+                _snake.flipX = false;
+            }
+            else
+                _stateSnake = MovementState.Stay;
+            if (Vector2.Distance(transform.localPosition, target.transform.localPosition) < 1 && CatSprite._stateCat != CatSprite.MovementState.hit)
+            {
+                if (transform.position.x > target.transform.position.x) _snake.flipX = false;
+                else _snake.flipX = true;
+                _stateSnake = MovementState.attake;
+            }
+            if (Vector3.Distance(transform.localPosition, target.transform.localPosition) < 1 && CatSprite._stateCat == CatSprite.MovementState.hit)
+            {
+                _stateSnake = MovementState.hurt;
+                HP -= 1;
+            }
+            _animator.SetInteger("state", (int)_stateSnake);
         }
-        else if (_rb.velocity.x < 0)
-        {
-            _state = MovementState.Walk;
-            _snake.flipX = false;
-        }
-        else
-            _state = MovementState.Stay;
-        if (Vector2.Distance(transform.localPosition, target.transform.localPosition) < 1)
-        {
-            if (transform.position.x > target.transform.position.x) _snake.flipX = false;
-            else _snake.flipX = true;
-            _state = MovementState.attake;
-        }
-        if (Vector3.Distance(transform.localPosition, target.transform.localPosition) < 1)
-        {
-            _state = MovementState.hurt;
-            HP -= 1;
-        }
-        _animator.SetInteger("state", (int)_state);
     }
 }
