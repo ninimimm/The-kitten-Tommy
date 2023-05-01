f26ae00a532438a1fc5e3e794281bec14d759fce
yellooot <njkzrhjkz@gmail.com>
2023-05-01 13:43:49 +0500

    Add scorpio

---
+++ Assets/Scripts/Scorpio.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using Unity.VisualScripting;
+
+public class Scorpio : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        
+    }
+}
