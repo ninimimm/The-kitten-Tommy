d8dccde95f1642068e3afe0248c20044b1e78a56
ninimimm <nikitachernov89221156246@yandex.ru>
2023-05-22 23:37:04 +0500

    Main to Trainer

--- Assets/MainMenu/MainMenu.cs
+++ Assets/MainMenu/MainMenu.cs
@@ -5,9 +5,14 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    
+    public void PlayTrainer()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
+    
     public void QuitGame()
     {
         Application.Quit();
